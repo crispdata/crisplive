228a932cdadeb373ddf6c54b6580d7b3b4934794
Suraj Sharma <suraj@gmail.com>
2019-02-04 20:39:40 +0530

    local

--- common/config/params.php
+++ common/config/params.php
@@ -5,6 +5,7 @@ return [
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
     'IAM_KEY' => 'AKIAJOYFSXBHL7QCKZ4Q',
+    
     'IAM_SECRET' => 'pCtVrBJ7Au2fS6Sj6VIv3rCGFlluzELz0Gucp8am',
     'BASE_URL' => 'https://crispdata.co.in/frontend/web/assets/images/',
     'ROOT_URL' => 'https://crispdata.co.in/backend/',
