5eeda3a38ff2036f701d229244c686bf540a4837
crispdata <47321374+crispdata@users.noreply.github.com>
2019-02-27 04:30:22 +0530

    Update params.php

--- common/config/params.php
+++ common/config/params.php
@@ -4,9 +4,8 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'IAM_KEY' => 'AKIAJOYFSXBHL7QCKZ4Q',
-    
-    'IAM_SECRET' => 'pCtVrBJ7Au2fS6Sj6VIv3rCGFlluzELz0Gucp8am',
+    'IAM_KEY' => '',
+    'IAM_SECRET' => '',
     'BASE_URL' => 'https://crispdata.co.in/frontend/web/assets/images/',
     'ROOT_URL' => 'https://crispdata.co.in/backend/',
 ];
