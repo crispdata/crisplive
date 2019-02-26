ee34060075e1030eab237481ef18cc5b71d9c928
crispdata <47321374+crispdata@users.noreply.github.com>
2019-02-27 04:25:07 +0530

    Update params.php

--- backend/config/params.php
+++ backend/config/params.php
@@ -6,5 +6,5 @@ return [
     'ROOT_URL' => 'https://crispdata.co.in/admin/index.php/',
     'IMAGE_URL' => 'https://crispdata.co.in/admin/',
     'bucketName' => 'aadhdata',
-    'IAM_KEY' => 'AKIAJOYFSXBHL7QCKZ4Q',
-    'IAM_SECRET' => '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',];
+    'IAM_KEY' => '',
+    'IAM_SECRET' => '',];
